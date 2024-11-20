b3844cfc4c02f30b58da20862c140df9a08decda
arodriguez33 <arodriguez@togetherdevs.com>
2024-11-20 12:34:06 -0300

    testimonials

--- src/sections/testimonials/Testimonials.tsx
+++ src/sections/testimonials/Testimonials.tsx
@@ -27,7 +27,7 @@ export default function Component() {
       py: 8,
       px: 4,
       // Cambia el color de fondo basado en el índice actual
-      bgcolor: currentIndex % 2 === 0 ? '#353535' : '#A0730C' 
+      bgcolor: currentIndex % 2 === 0 ? '#353535' : '#162D4F' 
     }}>
       <Swiper
         modules={[Navigation, Autoplay]}
@@ -62,7 +62,7 @@ export default function Component() {
                   />
                 ) : (
                   <Image 
-                    src="/assets/quotec.svg"
+                    src="/assets/quote.png"
                     alt="quote"
                     loading="lazy" 
                     width={110} 
@@ -78,7 +78,7 @@ export default function Component() {
                   mb: 3,
                   fontSize: { xs: '1.1rem', md: '1.25rem' },
                   lineHeight: 1.6,
-                  color: index % 2 === 0 ? '#FFFFFF': '#FFFFFF',
+                  color: index % 2 === 0 ? '#FFFFFF': '#ffffff',
 
                 }}
               >
@@ -87,7 +87,7 @@ export default function Component() {
               <Typography 
                 variant="subtitle1" 
                 sx={{ 
-                  color: index % 2 === 0 ? '#E8FE74': '#000000',
+                  color: index % 2 === 0 ? '#E8FE74': '#E8FE74',
                   fontWeight: 700,
                   mb: 0.5,
                   fontFamily: 'Raleway',
@@ -102,7 +102,7 @@ export default function Component() {
               <Typography 
                 variant="body2" 
                 sx={{ 
-                  color: index % 2 === 0 ? '#E8FE74': '#000000',
+                  color: index % 2 === 0 ? '#E8FE74': '#E8FE74',
                   fontWeight: 600,
                   mb: 0.5,
                   fontFamily: 'Raleway',
